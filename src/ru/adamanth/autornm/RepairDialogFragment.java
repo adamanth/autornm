ad9d5f863ca169aa96df0578277fc9e139b3ec70
adamanth <adamanth@pochtamt.ru>
2012-11-20 17:27:20 +0300

    Cosmetics

--- src/ru/adamanth/autornm/RepairDialogFragment.java
+++ src/ru/adamanth/autornm/RepairDialogFragment.java
@@ -4,10 +4,7 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 
 public class RepairDialogFragment extends DialogFragment {
 	
